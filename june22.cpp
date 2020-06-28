4a314a56094ccb2048d079d8b0a2046317532a68
Gunisha Chaturvedi <gunisha3007@gmail.com>
2020-06-28 22:46:41 +0530

    Add files via upload

---
+++ june22.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    vector <int> a={1,2,3,1,3};
+    // vector <int> frq(5,0);
+    // for (int i=0;i<5;i++)
+    // frq[a[i]]++;
+    
+    // for (int i=0;i<5;i++)
+    // cout<<frq[i];
+    
+    // unordered_map <int,int> mp;
+    // for (int i=0;i<5;i++)
+    // mp[a[i]]++;
+    
+    // for(auto i=mp.begin();i!=mp.end();i++)
+    // cout<<"key:"<<i->first<<"value:"<<i->second;
+    
+    pair <int,int> p;
+    vector <pair<int,int>> a={{1,2},{3,4}};
+for (int i=0;i<a.size();i++)
+cout<<a[i].first; return 0;
+}
