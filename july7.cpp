99563ffbd1f7c3d6aa3949a986d1e4a3ae790962
Gunisha Chaturvedi <gunisha3007@gmail.com>
2020-07-08 23:49:01 +0530

    Update july7.cpp

--- july7.cpp
+++ july7.cpp
@@ -1,3 +1,4 @@
+//ques 1
 priority_queue<int,vector<int>,greater<int>> pq;
 vector <int> ar={1,2,3,4,5};
 for (int i=0;i<ar.size();i++)
@@ -13,3 +14,30 @@ pq.pop();
 sum=first+sec;
 pq.push(sum);
 }
+
+//ques2
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int a=, b=, prev=0, next=0, tank=b, ans=0, i;
+  vector <int> c=
+  vector <int> d=
+  int n=c.size();
+  
+  priority_queue <int> q;
+  for (i=0;i<n;i++)
+  {
+    loc=c[i] //current location is c[i]th stn
+    if(a>c[i]) //not yet reached
+    {
+      tank = tank-(next-prev); //val of tank reduces because you have travelled further
+      while(!q.empty() && tank<0)
+      {
+        tank= tank+q.top(); //filling tank with max fuel
+        ans++;
+        q.pop();
+      }
+      if (tank<0)
+        return -1;
