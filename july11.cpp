9bc30c9ceca60f2ce79d369000e3a76d1bdc3899
Gunisha Chaturvedi <gunisha3007@gmail.com>
2020-07-10 13:33:26 +0530

    Update july11.cpp

--- july11.cpp
+++ july11.cpp
@@ -14,3 +14,35 @@ int main()
     cout<<a[n-1];
     return 0;
 }
+
+
+int main()
+{
+    vector <int> a={0,1,0,2,1,0,1};
+    int i,n=a.size(),min1=0;
+    vector <int> left (n,0), right(n,0);
+    for(i=1;i<n;i++)
+    {
+        min1=max(min1,a[i-1]);
+        left[i]=min1;
+    }
+    min1=0;
+    for(i=n-2;i>=0;i--)
+    {
+        min1=max(min1,a[i+1]);
+        right[i]=min1;
+    }
+    int ct=0;
+    for(i=0;i<n;i++)
+    {
+        if(min(left[i],right[i])>a[i])
+        ct=ct+(min(left[i],right[i])-a[i]);
+    }
+    cout<<ct;
+    for(i=1;i<n;i++)
+    cout<<left[i];
+    cout<<endl;
+    for(i=1;i<n;i++)
+    cout<<right[i];
+    return 0;
+}
